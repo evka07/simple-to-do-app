d2ec4720bced12e169f5565b874940edb9dec803
evka07 <138365894+evka07@users.noreply.github.com>
2023-10-23 11:47:13 +0200

    add favorite cards

--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -18,7 +18,7 @@ const Column = (props) => {
             </h2>
             <ul className={style.cards}>
                 {cards.map((card) => (
-                    <Card key={card.id} title={card.title} />
+                    <Card key={card.id} title={card.title}  id={card.id} isFavorite={card.isFavorite}/>
                 ))}
             </ul>
             <CardForm columnId={props.id} action={props.addCard} />
--- src/pages/Favorite/Favorite.jsx
+++ src/pages/Favorite/Favorite.jsx
@@ -2,15 +2,24 @@ import React from 'react';
 import {useSelector} from "react-redux";
 import {getFavorite} from "../../redux/store.js";
 import Card from "../../components/Card/Card.jsx";
+import style from './Favorite.module.scss'
 
 const Favorite = () => {
     const favoriteCards = useSelector(state => getFavorite(state))
     return (
-        <div>
+        <div className={style.favorite}>
             <h1>Favorite</h1>
-            {favoriteCards.map((card) => (
-                <Card key={card.id} id={card.id} title={card.title} isFavorite={card.isFavorite}/>
-            ))}
+            {favoriteCards.length === 0 ? (
+                <div className={style.noItem}>No Item</div>
+            ) : (
+                <nav className={style.navbar}>
+                    <ul className={style.cards}>
+                        {favoriteCards.map((card) => (
+                            <Card key={card.id} id={card.id} title={card.title} isFavorite={card.isFavorite}/>
+                        ))}
+                    </ul>
+                </nav>
+            )}
         </div>
     );
 };
